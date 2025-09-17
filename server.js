b1f6217447066d57e65e3d0d7e14f1132e6e4283
Mehmet <mathdenizi@gmail.com>
2025-09-17 06:11:38 +0200

    dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // React build klasörünü statik olarak sun
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 // Express 5 uyumlu regex
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
