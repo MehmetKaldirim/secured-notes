eca4e5663c527e6f1e87877b8342e6744c3e8c4a
Mehmet <mathdenizi@gmail.com>
2025-09-17 05:41:05 +0200

    Heroku deploy için server.js ve script güncellemesi

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+const PORT = process.env.PORT || 3000;
 
+// React build klasörünü statik olarak sun
+app.use(express.static(path.join(__dirname, "build")));
 // Express 5 uyumlu regex
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 React app running on port ${PORT}`);
 });
